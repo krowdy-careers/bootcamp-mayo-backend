3e1da625c9951a9ce27a289c24eafded2fbafb96
mizhac <cristhian@krowdy.co>
2022-05-06 18:20:58 -0500

    [update] add scripts

---
+++ src/internalapp/index.ts
@@ -0,0 +1,3 @@
+const [,, firstParams, secondParams] = process.argv;
+console.log('🚀 ~ file: index.ts ~ line 7 ~ secondParams', secondParams);
+console.log('🚀 ~ file: index.ts ~ line 7 ~ firstParams', firstParams);
