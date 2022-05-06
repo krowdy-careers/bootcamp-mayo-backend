03ce826c0893b700a51a801b2cf82d8141b493c7
mizhac <cristhian@krowdy.co>
2022-05-06 18:34:11 -0500

    [update] add last changes

--- src/internalapp/index.ts
+++ src/internalapp/index.ts
@@ -1,3 +1,33 @@
-const [,, firstParams, secondParams] = process.argv;
-console.log('🚀 ~ file: index.ts ~ line 7 ~ secondParams', secondParams);
-console.log('🚀 ~ file: index.ts ~ line 7 ~ firstParams', firstParams);
+
+import {spawn} from 'child_process';
+
+const [nodeParam, fileExecuteParam, firstParams, secondParams] = process.argv;
+
+
+const allParams = [firstParams, secondParams];
+
+const sendDataToOS = (params: string) => {
+  const principalCommand = 'echo';
+  const args = [`${params}`];
+  const options = {
+    shell: true,
+  };
+
+  const child = spawn(principalCommand, args, options);
+
+  child.stdout.on('data', (data) => {
+    console.log(`Output: ${data}`);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.log(`Error: ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log('🚀 ~ file: index.ts ~ line 25 ~ child.on ~ code', code);
+  });
+};
+
+for (const params of allParams) {
+  sendDataToOS(params);
+}
